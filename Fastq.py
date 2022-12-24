734b57280b4bc1a30f87628d8a54e4d3267cbfe9
Manasa Upadhyaya <kupadhyaya6@gatech.edu>
2022-12-23 20:28:17 -0500

    Final code for Fastq to fasta format

--- Fastq.py
+++ Fastq.py
@@ -8,7 +8,18 @@ for line in fastq:
     if re.search(pattern="[?]", string=line) is None and re.search(pattern="[+/]", string=line) is None:
         dict_fastq["Sequence"] = line.strip()
         list_dict_fastq.append(dict_fastq)
-print(list_dict_fastq)
+
+fasta_fastq = open("fasta_fastq.txt", "w")
+for list_items in list_dict_fastq:
+    fasta_desc = ""
+    fasta_seq = ""
+    for key, value in list_items.items():
+        if key != 'Sequence':
+            fasta_desc += ">" + list_items[key]
+
+        else:
+            fasta_seq += list_items[key].upper()
+            fasta_fastq.write(fasta_desc) + fasta_fastq.write("\n") + fasta_fastq.write(fasta_seq) + fasta_fastq.write("\n")
 
 
 
